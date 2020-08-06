9ab28258645f468309ee83962bc892a77131a31e
miguel <miguel.rueda@outlook.com>
2020-08-06 18:40:14 -0500

    Module 3 completed

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,29 +5,41 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 process.env.NODE_ENV = "development";
 
 module.exports = {
-    mode: "development",
-    target: "web",
-    devTool: "cheap-module-source-map",
-    entry: "./src/index",
-    output: {
-        path: path.resolve(__dirname, "build"),
-        publicpath: "/",
-        filename: "bundle.js"
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
     },
-    devServer: {
-        stats: "minimal",
-        overlay: true,
-        historyApiFallback: true,
-        disableHostCheck: true,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        },
-        https: false
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "src/index.html",
-            favicon: "src/favicon.ico"
-        })
-    ]
-}
+    https: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
