f088a10ae2e40de2abbfade2868fe2336e860ba1
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-08-12 09:52:39 -0500

    Module 11 completed

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,8 +5,13 @@ import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
+import {Redirect} from "react-router-dom";
 
 class CoursesPage extends React.Component {
+
+    state = {
+        redirectToAddCoursePage: false
+    };
 
     componentDidMount() {
         const { courses, authors, actions } = this.props;
@@ -25,7 +30,13 @@ class CoursesPage extends React.Component {
     render() {
         return (
             <>
+                {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
                 <h2>Courses</h2>
+                <button style={{marginBottom: 20}}
+                    className="btn btn-primary add-course"
+                    onClick={() => {this.setState({redirectToAddCoursePage: true})}} >
+                        Add Course
+                    </button>
                 <CourseList courses={this.props.courses} />
             </>
         );
@@ -39,7 +50,6 @@ CoursesPage.propTypes = {
 };
 
 function mapStateToProps(state) {
-    console.log(state.authors);
     return {
         courses: state.authors.length === 0 ? [] : state.courses.map(course => {
             return {
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,12 +1,16 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course };
-}
-
 export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function createCourseSUccess(course) {
+    return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+    return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -16,5 +20,18 @@ export function loadCourses() {
         }).catch(error => {
             throw error;
         });
+    }
+}
+
+export function saveCourse(course) {
+    return function (dispatch, getState) {
+        return courseApi.saveCourse(course)
+            .then(savedCourse => {
+                course.id ? 
+                    dispatch(updateCourseSuccess(savedCourse)) : 
+                    dispatch(createCourseSUccess(savedCourse));
+            }).catch(error => {
+                throw error;
+            });
     }
 }
